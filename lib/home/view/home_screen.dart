d8a75a67fd7b50ca30007152886aea82a3692908
Hyeonseok0627 <139304184+Hyeonseok0627@users.noreply.github.com>
2024-01-21 00:17:46 +0900

    home_screen.dart ListView.builder 활용한 페이지 5개 가로 방향으로 스크롤뷰 구현(240121 [00:17])

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,11 +1,33 @@
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200, // 스크롤 영역의 높이 설정
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal, // 수평 스크롤 활성화
+              itemCount: 5, // 페이지/아이템 수
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.of(context).size.width, // 각 페이지의 너비
+                  color: Colors.grey[300], // 시각적 구분을 위한 배경색
+                  child: Center(
+                    child: Text(
+                      '페이지 ${index + 1}',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          // HomeScreen의 다른 위젯들은 여기에 추가할 수 있습니다
+        ],
+      ),
     );
   }
 }
