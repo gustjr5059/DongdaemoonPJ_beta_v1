91541a29ccdf7c7f89a11f536df35eec89becd81
Hyeonseok0627 <139304184+Hyeonseok0627@users.noreply.github.com>
2024-01-21 00:31:24 +0900

    home_screen.dart PageView.builder 활용한 페이지 5개 가로 방향으로 스크롤뷰 구현 / 화살표를 활용하여 페이지 이동 구현 완료(240121 [00:31])

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,31 +1,58 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // 페이지 컨트롤러 초기화
+  final PageController _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           SizedBox(
-            height: 200, // 스크롤 영역의 높이 설정
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal, // 수평 스크롤 활성화
-              itemCount: 5, // 페이지/아이템 수
+            height: 200, // 페이지 뷰의 높이를 200으로 설정
+            // PageView.builder를 사용하여 스크롤 가능한 페이지 뷰 생성
+            child: PageView.builder(
+              controller: _controller, // 컨트롤러 할당
+              scrollDirection: Axis.horizontal, // 수평 스크롤 설정
+              itemCount: 5, // 총 페이지 수
               itemBuilder: (context, index) {
-                return Container(
-                  width: MediaQuery.of(context).size.width, // 각 페이지의 너비
-                  color: Colors.grey[300], // 시각적 구분을 위한 배경색
-                  child: Center(
-                    child: Text(
-                      '페이지 ${index + 1}',
-                      style: TextStyle(fontSize: 24),
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // 각 페이지의 컨텐츠
+                    Container(
+                      color: Colors.grey[300],
+                      child: Center(child: Text('페이지 ${index + 1}', style: TextStyle(fontSize: 24))),
                     ),
-                  ),
+                    // 왼쪽 화살표 버튼 (비활성화 상태로 표시)
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        color: index > 0 ? Colors.black : Colors.grey, // 비활성화 색상
+                        onPressed: index > 0 ? () => _controller.previousPage(duration: Duration(milliseconds: 300), curve: Curves.easeIn) : null,
+                      ),
+                    ),
+                    // 오른쪽 화살표 버튼 (비활성화 상태로 표시)
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        color: index < 4 ? Colors.black : Colors.grey, // 비활성화 색상
+                        onPressed: index < 4 ? () => _controller.nextPage(duration: Duration(milliseconds: 300), curve: Curves.easeIn) : null,
+                        ),
+                      ),
+                  ],
                 );
               },
             ),
           ),
-          // HomeScreen의 다른 위젯들은 여기에 추가할 수 있습니다
         ],
       ),
     );
