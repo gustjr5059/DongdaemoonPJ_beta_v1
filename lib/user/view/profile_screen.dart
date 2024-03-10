628765d1338366dd3bab6b74aa24b2be672b42e8
gustjr5059 <gustjr5059@naver.com>
2024-03-11 00:08:11 +0900

    cart_screen.dart, order_screen.dart, profile_screen.dart를 new_layout.dart 내용처럼 기본형식으로 리팩토링 작업완성(240311 [00:08])

--- lib/user/view/profile_screen.dart
+++ lib/user/view/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../common/provider/state_provider.dart';
@@ -11,15 +12,6 @@ class ProfileScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final tabIndex = ref.watch(tabIndexProvider);
 
-    // 페이지 컨트롤러 초기화
-    final PageController _controller = PageController();
-    int _currentPage = 0;
-
-    // 가상의 카테고리 데이터
-    final List<String> categories = List.generate(12, (index) => '카테고리 ${index + 1}');
-    String _selectedCategory = '카테고리 1';
-
-
     // TopBar 카테고리 리스트를 생성하고 사용자가 탭했을 때의 동작을 정의합니다.
     Widget topBarList = buildTopBarList(context, (index) {
       // 각 카테고리 인덱스에 따른 동작을 여기에 정의합니다.
@@ -30,37 +22,19 @@ class ProfileScreen extends ConsumerWidget {
     return Scaffold(
       // GlobalKey 제거
       // key: scaffoldKey, // common_parts.dart에서 정의한 GlobalKey 사용
-      appBar: buildCommonAppBar('마이페이지', context),// common_parts.dart의 AppBar 재사용
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // common_parts.dart에서 가져온 카테고리 리스트
-            Container(
-              height: 100, // TopBar의 높이 설정
-              child: topBarList, // 수정된 buildTopBarList 함수 호출
-            ),
-            SizedBox(
-              height: 200, // 페이지 뷰의 높이를 200으로 설정
-              child: PageView.builder(
-                controller: _controller,
-                scrollDirection: Axis.horizontal,
-                itemCount: 5, // 총 페이지 수
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: Colors.grey[300],
-                    child: Center(child: Text('페이지 ${index + 1}', style: TextStyle(fontSize: 24))),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 300, // 선택된 카테고리의 콘텐츠를 표시하는 영역
-              child: Center(
-                child: Text(_selectedCategory, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              ),
-            ),
-          ],
-        ),
+      appBar: buildCommonAppBar('PROFILE', context),// common_parts.dart의 AppBar 재사용
+      // body에 카테고리 리스트 포함
+      body: Column(
+        children: [
+          // common_parts.dart에서 가져온 카테고리 리스트
+          Container(
+            height: 100, // TopBar의 높이 설정
+            child: topBarList, // 수정된 buildTopBarList 함수 호출
+          ),
+          Expanded(
+            child: Center(child: Text('PROFILE 내용')),
+          ),
+        ],
       ),
       // buildCommonBottomNavigationBar 함수 호출 시 context 인자 추가
       bottomNavigationBar: buildCommonBottomNavigationBar(tabIndex, ref, context),
