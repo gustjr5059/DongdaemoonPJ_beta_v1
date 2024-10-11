4a89816d18bc8d4ee47ed419294bacd365a7e2cf
gustjr5059 <gustjr5059@naver.com>
2024-10-12 00:19:30 +0900

    [꾸띠르 프로젝트] 대배너 이미지 변경 작업 완료 / (앱 버전: 1.0.1(8)) (241012 [00:19])

--- lib/common/repository/banner_repository.dart
+++ lib/common/repository/banner_repository.dart
@@ -61,9 +61,9 @@ class AllLargeBannerRepository {
       Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
       // 데이터 맵에서 특정 키를 사용하여 이미지 URL을 가져온 후 AllLargeBannerImage 객체로 변환함.
       return [
-        AllLargeBannerImage.fromJson({'imageUrl': data['bb_test_5']}),
-        AllLargeBannerImage.fromJson({'imageUrl': data['bb_test_2']}),
-        AllLargeBannerImage.fromJson({'imageUrl': data['bb_test_3']})
+        AllLargeBannerImage.fromJson({'imageUrl': data['style22']}),
+        AllLargeBannerImage.fromJson({'imageUrl': data['style28']}),
+        AllLargeBannerImage.fromJson({'imageUrl': data['style29']})
       ].whereType<AllLargeBannerImage>().toList(); // 생성된 객체들을 List로 변환하여 반환함.
     }
     print('Failed to fetch large banner images: Document does not exist.');
